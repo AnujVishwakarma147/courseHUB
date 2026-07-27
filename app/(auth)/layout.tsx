e8348b36c5c70ebc48a483dbd8dde07327b48eea
Anuj Vishwakarma <av554289@gmail.com>
2026-07-27 17:55:43 +0530

    01:setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import  Logo from "@/public/logo.png"
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center px-4">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "outline" }),
+          "absolute left-4 top-4 gap-2 md:left-8 md:top-8"
+        )}
+      >
+        <ArrowLeft className="size-4" />
+        <span>Back</span>
+      </Link>
+
+      <div className="flex w-full max-w-sm flex-col gap-6">
+
+        <Link className="flex items-center gap-2 self-center font-medium" 
+        href="/">
+          <Image src={Logo} alt="Logo" width={32} height={32} />
+          MarshalLMS
+        </Link>
+
+        {children}
+
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "} <span className="hover:text-primary hover:underline">Terms of Service</span> and {" "}<span>Privacy Policy</span>.
+        </div>
+
+      </div>
+    </div>
+  );
+}
