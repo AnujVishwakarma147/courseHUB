a54973b1462d0ae01fc3286248d58457872e6e2c
Anuj Vishwakarma <av554289@gmail.com>
2026-07-28 01:18:35 +0530

    02:authentication system

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,37 +1,20 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { FaGithub } from "react-icons/fa";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 
-export default function LoginPage() {
+import {auth} from "@/lib/auth";
+import { LoginForm } from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+ 
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if(session){
+    return  redirect("/");
+  }
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>Login with your Github Email Account</CardDescription>
-      </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-            <Button className="w-full">
-                <FaGithub className="h-4 w-4" />
-                Sign in with GitHub
-            </Button>
-
-            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-0.5 after:z-0 after:flex after:items-center after:border-t after:border-border">
-              <span className="relative z-10 br-card px-2 text-muted-foreground">Or continue with</span>
-            </div>
-
-            <div className="grid gap-3">
-              <div className="flex flex-col space-y-1">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="name@example.com" />
-              </div>
-              <Button>Continue with Email</Button>
-
-            </div>
-
-        </CardContent>
-
-    </Card>
+   <LoginForm/>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { ThemeToggle } from "@/components/ui/themeToggle";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+  
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("Signed out successfully");
+        },
+        onError: () => {
+          toast.error("Internal Server Error");
+        },
+      },
+    });
+  }
+
   return (
-   <div>
-     <h1>Hello world</h1>
-     <ThemeToggle/>
-   </div>
+    <div>
+      <h1>Hello world</h1>
+      <ThemeToggle />
+      {session ? <div>
+        <p>{session.user?.name}</p>
+        <Button onClick={signOut}>Log Out</Button>
+      </div>
+: <Button onClick={() => router.push("/login")}>Login</Button>}
+
+    </div>
   );
 }
